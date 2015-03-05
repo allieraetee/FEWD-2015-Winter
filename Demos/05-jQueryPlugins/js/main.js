8aa357e12edf1608bfe72593d373950ce76b3a51
Allie Tscheulin <artscheulin@gmail.com>
2015-03-05 16:13:10 -0500

    commiting

---
+++ Demos/05-jQueryPlugins/js/main.js
@@ -0,0 +1,19 @@
+//this is the document, the html the copy yadda has to be loaded before scripts start
+
+$(document).ready(function() {
+
+	$(".list").highlight();
+
+});
+
+
+
+
+
+
+
+
+
+
+//this means the entire site- graphics and all must be loaded first
+//$(window).load(function(){  });
